c2e9ed702a5a521e85b023301010fae8b5581099
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-05-21 12:13:33 +0100

    add inference bash file

---
+++ inference.sh
@@ -0,0 +1,6 @@
+python \
+    src/tasks/inference.py \
+    --eval_model_dir models/pretrained_checkpoint/ \
+    --test_video_fname /mnt/welles/scratch/adrienne/FAVDBench/videos/inference/\
+    --do_lower_case \
+    --do_test \
