bb6247dd5cc6880929979d46969a39c7d4d436ca
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-05-22 17:37:49 +0100

    add ignore clips handling

--- data_prepro/metadata/generate_caption_tsv.py
+++ data_prepro/metadata/generate_caption_tsv.py
@@ -1,12 +1,19 @@
 import csv
 import json
 
-annotations_file = '/mnt/welles/scratch/adrienne/MAD/annotations/MAD-v1/MAD_test.json'
-output_file = '/home/adrienne/AutoMAD/datasets/test/test.label.tsv'
+annotations_file = '/mnt/welles/scratch/adrienne/MAD/annotations/MAD-v1/MAD_val.json'
+output_file = '/home/adrienne/AutoMAD/datasets/test/val.label.tsv'
+ignore_clips_file = '/home/adrienne/AutoMAD/datasets/archive-clips-under-8-frames/val-clips-under-8frames.txt'
 
 print('Loading cached annotations...')
 with open(annotations_file, 'r') as f:
     annotations = json.load(f)
+
+ignore_IDs = []
+print('Loading clip IDs to exclude from dataset...')
+with open(ignore_clips_file, 'r') as g:
+    for line in g:
+        ignore_IDs.append(int(line))
 
 # Convert annotations to a list of dictionaries for easier processing
 annotations_list = [{**{"id": key}, **value} for key, value in annotations.items()]
@@ -17,7 +24,11 @@ with open(output_file, 'w', newline='') as file:
 
     for annotation in annotations_list:
         annotation_ID = annotation['id']
-        caption = annotation['sentence']
-        caption_dict = [{'caption': caption}]
 
-        writer.writerow([annotation_ID, caption_dict])
+        if int(annotation_ID) in ignore_IDs:
+            continue
+        else:
+            caption = annotation['sentence']
+            caption_dict = [{'caption': caption}]
+
+            writer.writerow([annotation_ID, caption_dict])
