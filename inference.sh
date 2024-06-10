ae662732785673001aea96ef27a64fba17882a9d
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-06-10 19:58:22 +0100

    update config

--- inference.sh
+++ inference.sh
@@ -1,6 +1,8 @@
 python \
     src/tasks/inference.py \
-    --eval_model_dir models/pretrained_checkpoint/ \
-    --test_video_fname /mnt/welles/scratch/adrienne/FAVDBench/videos/inference/\
+    --eval_LMmodel_dir output/full-dataset-30ep/ \
+    --eval_ViTmodel_dir output/full-dataset-30ep/ \
+    --test_video_fname datasets/test/video_file.mp4 \
+    --test_features /datasets/test/8_frames_test_features.tsv \
     --do_lower_case \
     --do_test \
