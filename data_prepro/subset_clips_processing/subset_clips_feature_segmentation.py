e351b884ae8df0b952aa4632b8378abeb08dd7ac
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-06-10 19:52:15 +0100

    add subdataset processing

---
+++ data_prepro/subset_clips_processing/subset_clips_feature_segmentation.py
@@ -0,0 +1,82 @@
+import h5py
+import torch
+import csv
+import json
+
+def uniform_subsample(tensor, num_samples):
+    """
+    Uniformly subsample 'num_samples' frames from the tensor.
+    If the total_frames is less than num_samples, exclude from dataset.
+    """
+    total_frames = tensor.size(0)
+    if total_frames == 0 or total_frames < num_samples:
+        ## INPUT TEXT FILE NAME: ##
+        f = open("/home/adrienne/AutoMAD/datasets/test/test-clips-under-8frames.txt", "a")
+        f.write(annotation_ID)
+        f.write('\n')
+        f.close()
+        return None
+    indices = torch.linspace(0, total_frames - 1, num_samples).long()
+    return tensor[indices]
+
+## INPUT FILE DIRECTORIES: ##
+video_features_file = '/mnt/welles/scratch/adrienne/MAD/features/CLIP_B32_frames_features_5fps.h5'
+annotations_file = '/mnt/welles/scratch/adrienne/MAD/annotations/MAD-v1/MAD_test.json'
+output_file = '/home/adrienne/AutoMAD/datasets/32_clips_tsv/32_clips_test_features.tsv'
+
+## INPUT FRAME SAMPLING: ##
+frames_sampling = 8
+
+print('Loading cached annotations...')
+with open(annotations_file, 'r') as f:
+    annotations = json.load(f)
+
+# Convert annotations to a list of dictionaries for easier processing
+annotations_list = [{**{"id": key}, **value} for key, value in annotations.items()]
+movies = {a['movie']: a['movie_duration'] for a in annotations_list}
+
+print('Loading video features...')
+with h5py.File(video_features_file, 'r') as f:
+    video_feats = {m: torch.from_numpy(f[m][:]) for m in movies}
+
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='\t')
+
+    previous_movie_id = 0
+    annotation_count = 0  # Initialize annotation counter
+
+    for annotation in annotations_list:
+        if annotation_count >= 32:
+            break  ## STOP AFTER PROCESSING X ANNOTATIONS ##
+
+        annotation_ID = annotation['id']
+        movie_ID = annotation['movie']
+        start_timestamp, end_timestamp = annotation['timestamps']
+        movie_duration = annotation['movie_duration']
+
+        # Convert timestamps to frame indices
+        start_frame = int((start_timestamp / movie_duration) * len(video_feats[movie_ID]))
+        end_frame = int((end_timestamp / movie_duration) * len(video_feats[movie_ID]))
+
+        try:
+            numeric_id = int(annotation_ID)
+        except ValueError:
+            print("Invalid ID: not a number!")
+            continue
+
+        if movie_ID != previous_movie_id:
+            print(f'Processing movie ID: {movie_ID}...')
+        
+        full_feature_tensor = video_feats[movie_ID]
+        segmented_feature_tensor = full_feature_tensor[start_frame:end_frame + 1]
+        subsampled_feature_tensor = uniform_subsample(segmented_feature_tensor, frames_sampling)
+
+        if subsampled_feature_tensor is None:
+            continue
+
+        # Write annotation ID and frame tensors in one line
+        row = [annotation_ID] + [frame_tensor.numpy().tolist() for frame_tensor in subsampled_feature_tensor]
+        writer.writerow(row)
+
+        previous_movie_id = movie_ID
+        annotation_count += 1  # Increment the annotation counter
