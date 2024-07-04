d459b0ad456be93fb5395b95630ff5dd1566d6b0
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-07-04 11:04:32 +0100

    update config

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 ## Multiple GPUs ##
-torchrun --nproc_per_node=4 \
+CUDA_VISIBLE_DEVICES=1,2,3 torchrun --nproc_per_node=3 \
     ./src/tasks/train.py \
     --config ./src/configs/8frames_default.json \
-    --per_gpu_train_batch_size 1 \
-    --per_gpu_eval_batch_size 1 \
-    --num_train_epochs 100 \
+    --per_gpu_train_batch_size 8 \
+    --per_gpu_eval_batch_size 8 \
+    --num_train_epochs 30 \
     --learning_rate 0.0001\
     --max_num_frames 8 \
     --backbone_coef_lr 0.05 \
     --loss_sparse_w 0.5 \
-    --output_dir ./output/full-Batch-Size-1-ep100/ \
+    --output_dir ./output/full-Batch-Size-8-ep30/ \
